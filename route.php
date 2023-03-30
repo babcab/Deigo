bfa7e68740023877b4d9a5a65b1e421915fcd305
Tubaher <tubarao0705@gmail.com>
2023-03-30 17:25:51 -0500

    feat(checkpoints_between_six_minutes): wrap up function to get checkpoints between six minutes from the route

--- route.php
+++ route.php
@@ -30,10 +30,10 @@ function getRoute($url, $p_lat, $p_long, $d_lat, $d_long, $api_key)
 /* 
 Once we have the route, we can get the duration of the route in seconds, the distance in meters, and the steps of the route.
 */
-$directions = getRoute($url, "-0.1383219181126606", "-78.47949502180954", "-0.13780693548696138", "-78.48749873306527", "test");
-$duration = $directions['routes'][0]['legs'][0]['duration']['value'];
-$distance = $directions['routes'][0]['legs'][0]['distance']['value'];
-$steps = $directions['routes'][0]['legs'][0]['steps'];
+// $directions = getRoute($url, "-0.1383219181126606", "-78.47949502180954", "-0.13780693548696138", "-78.48749873306527", "test");
+// $duration = $directions['routes'][0]['legs'][0]['duration']['value'];
+// $distance = $directions['routes'][0]['legs'][0]['distance']['value'];
+// $steps = $directions['routes'][0]['legs'][0]['steps'];
 
 
 /* 
@@ -58,7 +58,7 @@ function get_nodes($steps)
     return $nodes;
 }
 
-$nodes = get_nodes($steps);
+// $nodes = get_nodes($steps);
 
 /* 
 Once we get the nodes, we can format them to be used in the distance matrix.
@@ -109,17 +109,17 @@ function get_distance_matrix($url, $origins, $destinations, $api_key)
 The distance matrix give us the duration in seconds and the distance in meters from the node of the route to the checkpoints.
 Note: The documentation says that we should not use multiple origins, so I'm iterating over each checkpoint.
 */
-$distance_matrix = get_distance_matrix($distance_matrix_url, ["-0.13834337572182936,-78.47949502180954"], $nodes, "test");
+// $distance_matrix = get_distance_matrix($distance_matrix_url, ["-0.13834337572182936,-78.47949502180954"], $nodes, "test");
 
 
 /* 
 checks if the duration of one checkpoint to the route is less than 6 minutes.
 */
-function is_between_six_minutes($distance_matrix)
+function is_between_minutes($distance_matrix, $minutes)
 {
     $times_from_route = $distance_matrix['rows'][0]['elements'];
     foreach ($times_from_route as $time) {
-        if ($time['duration']['value'] > 360) {
+        if ($time['duration']['value'] > $minutes * 60) {
             return false;
         }
     };
@@ -129,20 +129,30 @@ function is_between_six_minutes($distance_matrix)
 /* 
 Check if there is a checkpoint between 6 minutes from the route and save them in an array.
 */
-function checkpoints_between_six_minutes($checkpoints, $route, $distance_matrix_url)
+function checkpoints_between_minutes($checkpoints, $route, $minutes, $distance_matrix_url)
 {
     $checkpoints_between_six_minutes = [];
     foreach ($checkpoints as $checkpoint) {
         $distance_matrix = get_distance_matrix($distance_matrix_url, [$checkpoint], $route, "test");
-        if (is_between_six_minutes($distance_matrix)) {
+        if (is_between_minutes($distance_matrix, $minutes)) {
             $checkpoints_between_six_minutes[] = $checkpoint;
         }
     }
     return $checkpoints_between_six_minutes;
 }
 
-$checkpoints = ["-0.13957718821592419,-78.48562118730912", "-0.1428717384263475,-78.49239528170591"];
-checkpoints_between_six_minutes($checkpoints, $nodes, $distance_matrix_url);
+// $checkpoints = ["-0.13957718821592419,-78.48562118730912", "-0.1428717384263475,-78.49239528170591"];
+// checkpoints_between_six_minutes($checkpoints, $nodes, $distance_matrix_url);
 
 
 /* I made everything inside functions so you can reuse them when you need */
+
+
+function get_checkpoints_between_six_mins_to_route($checkpointsList, $pLat, $pLng, $dLat, $dLng, $distance_matrix_api_url, $maps_api_url,  $apiKey)
+{
+    $main_route = getRoute($maps_api_url, $pLat, $pLng, $dLat, $dLng, $apiKey);
+    $steps = $main_route['routes'][0]['legs'][0]['steps'];
+    $nodes = get_nodes($steps);
+    $checkpointsListWithTime = checkpoints_between_minutes($checkpointsList, $nodes, 6, $distance_matrix_api_url);
+    return $checkpointsListWithTime;
+}
