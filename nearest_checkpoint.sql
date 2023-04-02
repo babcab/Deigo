a97cf86b95ed7e4480a24946bf68fde6cff19684
Tubaher <tubarao0705@gmail.com>
2023-04-02 07:22:24 -0500

    feat(checkpoints): Refactor to use JSON data type in nearest checkpoints

--- nearest_checkpoint.sql
+++ nearest_checkpoint.sql
@@ -1,33 +1,56 @@
-CREATE FUNCTION find_nearest_checkpoints(lat FLOAT, lng FLOAT) RETURNS TABLE (
-    chk_id INT,
-    chk_lat FLOAT,
-    chk_lng FLOAT,
-    distance FLOAT
-) AS $$ BEGIN RETURN QUERY
-SELECT
-    checkpoint_id,
-    checkpoint_lat,
-    checkpoint_lng,
-    (
-        1.45 * 6371 * 2 * ASIN(
-            SQRT(
-                POWER(SIN(RADIANS(checkpoint_lat - lat) / 2), 2) + COS(RADIANS(lat)) * COS(RADIANS(checkpoint_lat)) * POWER(SIN(RADIANS(checkpoint_lng - lng) / 2), 2)
-            )
-        )
-    ) AS distance
-FROM
-    checkpoints
-WHERE
-    (
-        1.45 * 6371 * 2 * ASIN(
-            SQRT(
-                POWER(SIN(RADIANS(checkpoint_lat - lat) / 2), 2) + COS(RADIANS(lat)) * COS(RADIANS(checkpoint_lat)) * POWER(SIN(RADIANS(checkpoint_lng - lng) / 2), 2)
-            )
-        )
-    ) <= 1.0
-ORDER BY
-    distance ASC;
+DELIMITER $$
+CREATE FUNCTION get_distance(pLat FLOAT, pLog FLOAT, dLat FLOAT, dLog FLOAT) 
+RETURNS FLOAT 
+DETERMINISTIC 
+BEGIN 
+
+DECLARE distance FLOAT;
+SET
+  distance = (
+    1.45 * 6371 * 2 * ASIN(
+      SQRT(
+        POWER(SIN(RADIANS(dLat - pLat) / 2), 2) + COS(RADIANS(pLat)) * COS(RADIANS(dLat)) * POWER(SIN(RADIANS(dLog - pLog) / 2), 2)
+      )
+    )
+  );
+
+RETURN (distance);
 
 END;
+$$
 
-$$ LANGUAGE plpgsql
+DELIMITER $$
+CREATE FUNCTION get_nearest_checkpoints(user_lat FLOAT, user_log FLOAT, checkpoints_list JSON, distance FLOAT) 
+RETURNS JSON
+DETERMINISTIC
+BEGIN 
+  DECLARE nearest_checkpoints JSON;
+	SELECT JSON_ARRAYAGG(JSON_OBJECT('lat', distance_table.lat, 'log', distance_table.log)) INTO nearest_checkpoints FROM (
+		SELECT
+			lat,
+			log,
+			get_distance(user_lat, user_log, lat, log) as distance
+		FROM
+			JSON_TABLE(
+			checkpoints_list,
+			'$[*]' COLUMNS (lat FLOAT PATH '$.lat', log FLOAT PATH '$.log')
+			) jt
+		WHERE 
+			get_distance(user_lat, user_log, lat, log) <= distance
+		) distance_table;
+	RETURN nearest_checkpoints;
+END;
+$$
+
+
+/* 
+
+You can test this code usign this call 
+
+SELECT get_nearest_checkpoints(-0.13819999092199575, 
+    -78.47963368378458,
+    checkpoints, 1.0)as nearest_checkpoints 
+FROM  rides;
+
+I left an example of the data for checkpoints should look in test.json file
+ */
